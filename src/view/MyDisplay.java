0a3847eb0ec6ede5f34ea2f05338572c14351bbe
HugoJD <hugo.joliondavid@gmail.com>
2021-12-14 14:58:06 +0100

    paintComponent

---
+++ src/view/MyDisplay.java
@@ -0,0 +1,24 @@
+package view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class MyDisplay extends JPanel{
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawLine(50, 50, 250, 250);
+		System.out.println("drawLine");
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Java Avance - Graphic Display");
+		frame.setSize(new Dimension(500,500));
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		MyDisplay d = new MyDisplay();
+		frame.add(d);
+	}
+}
